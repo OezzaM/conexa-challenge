5a1ccd30a42f1187be114aab295c9176d7033130
Octavio <octaviomazzeo@gmail.com>
2023-10-17 03:28:07 -0300

    add name in swagger and delele empty folders

--- back/src/main.ts
+++ back/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document, {
-    customSiteTitle: 'Backend Generator',
+    customSiteTitle: 'Conexa challenge',
     customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
     customJs: [
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
